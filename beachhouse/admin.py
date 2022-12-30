d9e3792057684888bebef7e11ce05bacb2582ae9
CBergane <christian.bergane@gmail.com>
2022-12-30 23:35:57 +0000

    added some custom outlooks for admin page

--- beachhouse/admin.py
+++ beachhouse/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from .models import Bookings, House, Owner
 
 
-admin.site.register(Bookings)
-admin.site.register(House)
 admin.site.register(Owner)
+
+# custum made admin outlook
+
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'adress', 'owner')
+    ordering = ('name',)
+    search_fields = ('name', 'adress')
+
+
+@admin.register(Bookings)
+class BookingAdmin(admin.ModelAdmin):
+    fields = (('user', 'house'), 'checkin', 'checkout')
+    list_display = ('user', 'checkin', 'checkout')
+    list_filter = ('checkin', 'house')
+    ordering = ('checkin',)
