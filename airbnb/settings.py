17c5181c1081d7e41a6edb5d0e030ab55ec77ba5
CBergane <christian.bergane@gmail.com>
2023-01-08 18:16:35 +0000

    added style and custum lock for login and log out and some extra shadow on most cards

--- airbnb/settings.py
+++ airbnb/settings.py
@@ -90,21 +90,10 @@ TEMPLATES = [
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
+
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SOCIALACCOUNT_PROVIDERS = {
-    'github': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
-    }
-}
 
 WSGI_APPLICATION = 'airbnb.wsgi.application'
 
