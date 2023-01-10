54a799ae68c0513d5bec0d3eb8fd74d1014a4587
CBergane <christian.bergane@gmail.com>
2023-01-10 21:06:59 +0000

    added price to house model

--- beachhouse/forms.py
+++ beachhouse/forms.py
@@ -11,13 +11,14 @@ from django.core import validators
 class HouseForm(ModelForm):
     class Meta:
         model = House
-        fields = ('name', 'adress', 'owner', 'beds', 'capacity', 'description', 'house_image',)
+        fields = ('name', 'adress', 'owner', 'beds', 'capacity', 'price', 'description', 'house_image',)
         labels = {
             'name': '',
             'adress': '',
             'owner': 'Owner of the house',
             'beds': 'Number of beds',
             'capacity': 'Number of guests',
+            'price': 'Price per night',
             'description': '',
             'house_image': 'Add a picture of your house',
         }
@@ -27,6 +28,7 @@ class HouseForm(ModelForm):
             'owner': forms.Select(attrs={'class':'form-select', 'placeholder':'Owner of the house'}),
             'beds': forms.NumberInput(attrs={'class':'form-range', 'id':'beds', 'type':'range', 'min':'0', 'max':'10', 'placeholder':'Number of guests'}),
             'capacity': forms.NumberInput(attrs={'class':'form-range', 'id': 'capacity', 'type':'range', 'min':'0', 'max':'10', 'placeholder':'Number of guests'}),
+            'price': forms.NumberInput,
             'description': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Describe the house'}),
             'house_image': forms.FileInput(attrs={'class':'form-control','type':'file', 'placeholder':'House Name'}),
         }
