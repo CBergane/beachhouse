9dd2be8a6a254baa729df8d5382ba18d565f9a32
CBergane <christian.bergane@gmail.com>
2023-01-16 16:09:32 +0000

    added so calculating the price in realtime only loads on the correct page and dont get any error warnings

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
  // Get the check-in date input element and the book button
- const checkInDate = document.getElementById("id_checkin");
+const checkInDate = document.getElementById("id_checkin");
 const checkOutDate = document.getElementById("id_checkout");
 const bookButton = document.getElementById("book");
 if(checkInDate && checkOutDate && bookButton){
@@ -40,15 +40,18 @@ $(document).ready(function() {
     })
 })
 
-function calculateTotalPrice() {
-    let checkInDate = new Date(document.getElementById("id_checkin").value);
-    let checkOutDate = new Date(document.getElementById("id_checkout").value);
-    let housePrice = document.getElementById("price").innerHTML;
-    let oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-    let diffDays = Math.round(Math.abs((checkOutDate.getTime() - checkInDate.getTime())/(oneDay)));
-    let totalCost = diffDays * housePrice;
-    document.getElementById("total_cost").innerHTML = totalCost;
-}
+const price = document.getElementById("price");
+if(price) {
+    function calculateTotalPrice() {
+        let checkInDate = new Date(document.getElementById("id_checkin").value);
+        let checkOutDate = new Date(document.getElementById("id_checkout").value);
+        let housePrice = price.innerHTML;
+        let oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+        let diffDays = Math.round(Math.abs((checkOutDate.getTime() - checkInDate.getTime())/(oneDay)));
+        let totalCost = diffDays * housePrice;
+        document.getElementById("total_cost").innerHTML = totalCost;
+    }
 
-document.getElementById("id_checkin").addEventListener("change", calculateTotalPrice);
-document.getElementById("id_checkout").addEventListener("change", calculateTotalPrice);
+    document.getElementById("id_checkin").addEventListener("change", calculateTotalPrice);
+    document.getElementById("id_checkout").addEventListener("change", calculateTotalPrice);
+}
