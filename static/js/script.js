25c50616b9aaebede1d7e9e529bf315cfb0c6a47
CBergane <christian.bergane@gmail.com>
2023-01-29 16:12:05 +0000

    added a map function to display a location of an office

--- static/js/script.js
+++ static/js/script.js
@@ -186,3 +186,17 @@ function resetForm(){
     document.getElementById("has_bath").checked = false;
     document.getElementById("filterForm").submit();
   }
+
+function initMap() {
+    const myLatLng = { lat: 56.6484407, lng: 12.885896 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: myLatLng });
+    
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "Our office.",
+    });
+}
+window.initMap = initMap;
